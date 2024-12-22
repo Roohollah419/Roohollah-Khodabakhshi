113d9455e6e03f0de1a44ad55f4a9c362c3ea11f
Roohollah Khodabakhshi <roohollah.khodabakhshi@specific-group.com>
2024-12-22 10:40:17 +0330

    Auto-generated commit

--- chunk-3LFW67YN.js
+++
@@ -1,1 +0,0 @@
-import{R as n,ma as o,na as i,ra as s,sa as a}from"./chunk-YRVYHDRQ.js";var m=(()=>{class e{static{this.\u0275fac=function(t){return new(t||e)}}static{this.\u0275cmp=n({type:e,selectors:[["app-skills"]],standalone:!0,features:[a],decls:2,vars:0,template:function(t,r){t&1&&(o(0,"p"),s(1,"skills works!"),i())}})}}return e})();export{m as default};

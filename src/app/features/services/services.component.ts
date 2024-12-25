5e9f00140f2c0bf579849a2a9b74b598a5cc1ab4
Roohollah Khodabakhshi <roohollah.khodabakhshi@specific-group.com>
2024-12-25 14:03:01 +0330

    add services that I provide to customers

--- src/app/features/services/services.component.ts
+++ src/app/features/services/services.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule],
   templateUrl: './services.component.html',
-  styleUrl: './services.component.css'
+  styleUrl: './services.component.css',
 })
 export default class ServicesComponent {
-
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['<', '>'],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 2,
+      },
+      740: {
+        items: 3,
+      },
+      940: {
+        items: 4,
+      },
+    },
+    nav: true,
+  };
 }

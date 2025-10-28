6bcab4094f6b07ead2c90b9a4b3110b706928f8e
Roohollah Khodabakhshi <roohollah.khodabakhshi@specific-group.com>
2025-10-28 10:01:45 +0330

    Add agents and about me and contact components

--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -1,12 +1,34 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import saveAs from 'file-saver';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
 export default class ContactComponent {
+  contact = new FormGroup({
+    fullName: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', Validators.required),
+  });
 
+  submitForm = () => {
+    const val = this.contact.getRawValue();
+    console.log(val);
+    const blob = new Blob(
+      [val.email ?? '', '\n', val.fullName ?? '', '\n', val.message ?? ''],
+      { type: 'text/plain;charset=utf-8' }
+    );
+    saveAs(blob, 'save-me.txt');
+  };
 }
